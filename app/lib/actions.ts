f5f62a902eaefe59e69f39b142ae480cd31eb7a4
Vinicius Beritica <vinicius.beritica@rarolabs.com.br>
2025-03-17 17:48:29 -0300

    fix: corrige erros de lint

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -58,7 +58,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
